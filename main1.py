45783aa896b7f4565c67e7dec262bbe8159105ed
Adrianooliveira16 <157659650+Adrianooliveira16@users.noreply.github.com>
2024-01-31 21:06:16 -0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,26 @@
+#concatenação == junta
+
+numero = 1
+texto = 'text'
+
+print(numero,texto)
+
+a = 10
+b = (30 + 50)
+c = 50
+
+numero = 1
+n1 = 'texto'
+numero = 2
+n2 = 'texto 2'
+print(numero,n1,numero,n2,) 
+
+
+nome = 'adriano'
+idade = 35
+email = ('adriano@hotmail.com')
+nunero1 = 1
+numero2 = 2
+
+print('ola meu nome é',nome,'minha idede é',idade,'meu email é',email,)
+
